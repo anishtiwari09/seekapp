86e70a72236eb1a1a6fe41f883dee2ad106ecf12
unknown <aneeshtiwari24@gmail.com>
2024-05-12 02:55:08 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Component/Header/Header";
+
+function App() {
+  return (
+    <div className="main_container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Header/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="header_container">
+      <h1 className="title">Where in the world?</h1>
+      <div>
+        <button>
+          <img src={"/"}
+        </button>
+
+      </div>
+    </div>
+  );
+}
